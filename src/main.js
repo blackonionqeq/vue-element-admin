4598d166f36c5a5c9efbbef9584fcc4e3df070a3
Pan <panfree23@gmail.com>
2018-09-07 17:55:12 +0800

    init

--- src/main.js
+++ src/main.js
@@ -21,8 +21,10 @@ import './mock' // simulation data
 
 import * as filters from './filters' // global filters
 
+import defaultSettings from './settings'
+
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium', // set element-ui default size
+  size: Cookies.get('size') || defaultSettings.size, // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
 
---
+++ src/settings.js
@@ -0,0 +1,10 @@
+const defaultSettings = {
+  lang: 'zh', //  options: ['en', 'zh']
+  size: 'medium', //  options: ['medium', 'small','mini']
+  permission: true,
+  tagsView: true,
+  i18n: true,
+  viewTransition: 'fade-transform' // options:['fade-transform','fade']
+}
+
+export default defaultSettings
